bd99de66e2ea11a85155497195f34cac08b8369f
Josh Lawrence <joshlawrence@quadax.com>
2024-12-29 05:23:16 -0500

    feat: added setup.sh which auto installs configs

---
+++ setup.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+local_config_dir="$HOME/.config"
+remote_config_dir="./config"
+local_dirs=$(find $local_config_dir -mindepth 1 -maxdepth 1 -type d)
+remote_dirs=$(find "./config" -mindepth 1 -maxdepth 1 -type d)
+
+# Colors for string formatting
+Color_Off='\033[0m'
+Green='\033[0;32m'
+Yellow='\033[0;33m'
+Red='\033[0;31m'
+
+strip_dir_name(){
+    # Store default IFS value for restore later
+    old_IFS=$IFS
+
+    # Dont add this repo to scope
+    if [ $1 == "./.git" ]; then
+        return
+    fi
+
+    # Bash version of a string split
+    IFS='/'
+    read -ra name <<< $1
+
+    # Restore IFS back to avoid side effects
+    IFS=$old_IFS
+
+    # Last element should be target dir name
+    echo "${name[-1]}"
+}
+
+fetch_dirs(){
+    names=()
+
+    for dir in $1; do
+        name=$(strip_dir_name $dir)
+        names+=("${name}")
+    done
+
+    echo "${names[@]}"
+}
+
+detect_untracked(){
+    for i in $1; do
+        if [ $i == "nvim" ]; then
+            continue
+        fi
+
+        tracked=false
+
+        for e in $2; do
+            if [ $i == $e ]; then
+                tracked=true
+                break
+            fi
+        done
+
+        if [ $tracked == false ]; then
+        echo -e "$Yellow[WARNING]$Color_Off local config '$i' is not tracked"
+        fi
+    done
+}
+
+# Collect local and remote config dir names
+remote_names=$(fetch_dirs "${remote_dirs}")
+local_names=$(fetch_dirs "${local_dirs}")
+
+# Provision config
+for name in $remote_names; do
+    rm -rf "$local_config_dir/$name"
+    rsync -rvc --delete --quiet "$remote_config_dir/$name/" "$local_config_dir/$name/"
+
+    # Check for bad exit code by rsync
+    if [ $? -ne 0 ]; then
+        echo -e "$Red[Failed]$Color_Off local config '$name' sync."
+        exit
+    fi
+
+    echo -e "$Green[SUCCESS]$Color_Off local config '$name' is in sync with remote."
+done
+
+# Show local dirs not present in remote
+detect_untracked "${local_names}" "${remote_names}"
+
+# Done!
+echo -e "\n$Green[Complete]$Color_Off configuration syncd"
