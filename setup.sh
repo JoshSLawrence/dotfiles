4290f1dd303f07b98827d475ddaed347ae882757
Josh Lawrence <josh.stephen.lawrence@gmail.com>
2024-12-29 22:33:49 -0500

    feat: added package installs, shell changes, general formatting

--- setup.sh
+++ setup.sh
@@ -1,18 +1,54 @@
 #!/bin/bash
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#           _           _       _                                                 #
+#          | |         | |     | |                                                #
+#          | | ___  ___| |__   | |     __ ___      ___ __ ___ _ __   ___ ___      #
+#      _   | |/ _ \/ __| '_ \  | |    / _` \ \ /\ / / '__/ _ \ '_ \ / __/ _ \     #
+#     | |__| | (_) \__ \ | | | | |___| (_| |\ V  V /| | |  __/ | | | (_|  __/     #
+#      \____/ \___/|___/_| |_| |______\__,_| \_/\_/ |_|  \___|_| |_|\___\___|     #
+#                            _       _    __ _ _                                  #
+#                           | |     | |  / _(_) |                                 #
+#                         __| | ___ | |_| |_ _| | ___  ___                        #
+#                        / _` |/ _ \| __|  _| | |/ _ \/ __|                       #
+#                       | (_| | (_) | |_| | | | |  __/\__ \                       #
+#                        \__,_|\___/ \__|_| |_|_|\___||___/                       #
+#                                                                                 #
+#                                                                                 #
+#                             Josh Lawrence - dotfiles                            #
+#                                                                                 #
+#                        - https://github.com/JoshSLawrence                       #
+#                        - josh@joshlawrence.dev                                  #
+#                                                                                 #
+#                                                                                 #
+#                You can die anytime, but living takes true courage               #
+#                                                                                 #
+#                                                    - Kenshin Himura             #
+#                                                                                 #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
 cd "$(dirname "$0")"
 
 local_config_dir="$HOME/.config"
 remote_config_dir="./config"
 local_dirs=$(find $local_config_dir -mindepth 1 -maxdepth 1 -type d)
 remote_dirs=$(find "./config" -mindepth 1 -maxdepth 1 -type d)
-remote_root_dotfiles=$(find "./root" -mindepth 1 -maxdepth 1 -type f)
+remote_root_dotfiles=$(find "./root" -mindepth 1 -maxdepth 1)
 
 # Colors for string formatting
 Color_Off='\033[0m'
 Green='\033[0;32m'
 Yellow='\033[0;33m'
 Red='\033[0;31m'
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#         A bunch of helper functions - search for "Lets get shit installed"      # 
+#                          to see main execution area                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 
 strip_dir_name(){
     # Store default IFS value for restore later
@@ -67,6 +103,29 @@ detect_untracked(){
 }
 
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                          Lets get shit installed                                #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+# Install apt packages
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install build-essential # required for oh-my-posh
+sudo apt install zsh-syntax-highlighting
+sudo apt install zsh
+echo "Changing default shell to zsh, prompting for password..."
+chsh -s /bin/zsh
+
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# Install brew packages
+brew install jandedobbeleer/oh-my-posh/oh-my-posh
+brew install jesseduffield/lazygit/lazygit
+
 # Collect local and remote config dir names
 remote_names=$(fetch_dirs "${remote_dirs}")
 local_names=$(fetch_dirs "${local_dirs}")
@@ -75,7 +134,12 @@ remote_root_dotfile_names=$(fetch_dirs "${remote_root_dotfiles}")
 # Install root dotfiles
 for dotfile in $remote_root_dotfile_names; do
     rm -rf "$HOME/.$dotfile"
-    rsync -rvc --quiet "./root/$dotfile" "$HOME/.$dotfile"
+
+    if [ $dotfile == "tmux" ]; then
+        rsync -rvc "./root/$dotfile/" "$HOME/.$dotfile"
+    else
+        rsync -rvc "./root/$dotfile" "$HOME/.$dotfile"
+    fi
 
     # Check for bad exit code from copy
     if [ $? -ne 0 ]; then
@@ -89,7 +153,7 @@ done
 # Provision config
 for name in $remote_names; do
     rm -rf "$local_config_dir/$name"
-    rsync -rvc --delete --quiet "$remote_config_dir/$name/" "$local_config_dir/$name/"
+    rsync -rvc --delete "$remote_config_dir/$name/" "$local_config_dir/$name/"
 
     # Check for bad exit code by rsync
     if [ $? -ne 0 ]; then
@@ -104,4 +168,5 @@ done
 detect_untracked "${local_names}" "${remote_names}"
 
 # Done!
-echo -e "\n$Green[Complete]$Color_Off configuration syncd"
+echo -e "\n$Green[Complete]$Color_Off configuration syncd."
+echo -e "\nDon't forget to logout to change shells or source your ~/.zshrc!"
