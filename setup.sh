92899b2e60c43f1d0d5c09793da0595764d3a0c0
Josh Lawrence <josh.stephen.lawrence@gmail.com>
2024-12-30 09:53:55 -0500

    feat: added MacOS support

--- setup.sh
+++ setup.sh
@@ -51,23 +51,12 @@ Red='\033[0;31m'
 
 
 strip_dir_name(){
-    # Store default IFS value for restore later
-    old_IFS=$IFS
-
     # Dont add this repo to scope
     if [ $1 == "./.git" ]; then
         return
     fi
 
-    # Bash version of a string split
-    IFS='/'
-    read -ra name <<< $1
-
-    # Restore IFS back to avoid side effects
-    IFS=$old_IFS
-
-    # Last element should be target dir name
-    echo "${name[-1]}"
+    echo "$(basename "$1")"
 }
 
 fetch_dirs(){
@@ -107,22 +96,35 @@ detect_untracked(){
 #                          Lets get shit installed                                #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+OS=$(uname)
 
-# Install apt packages
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install build-essential # required for oh-my-posh
-sudo apt install zsh-syntax-highlighting
-sudo apt install zsh
-echo "Changing default shell to zsh, prompting for password..."
-chsh -s /bin/zsh
+if [ $OS == "Linux" ]; then
+    # Install apt packages
+    sudo apt update -y
+    sudo apt upgrade -y
+    sudo apt install build-essential # required for oh-my-posh
+    sudo apt install zsh-syntax-highlighting
+    sudo apt install zsh
 
-# Install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    # Install homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+    test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-# Install brew packages
+    echo "Changing default shell to zsh, prompting for password..."
+    chsh -s /bin/zsh
+elif [ $OS == "Darwin" ]; then
+    brew update
+    brew upgrade 
+    brew install ca-certificates
+    brew install curl
+    brew install zsh-syntax-highlighting
+else
+    echo "$Red[ERROR]$Color_Off unknown operating system!"
+fi
+
+
+# Install shared brew packages
 brew install jandedobbeleer/oh-my-posh/oh-my-posh
 brew install jesseduffield/lazygit/lazygit
 
@@ -130,7 +132,6 @@ brew install jesseduffield/lazygit/lazygit
 remote_names=$(fetch_dirs "${remote_dirs}")
 local_names=$(fetch_dirs "${local_dirs}")
 remote_root_dotfile_names=$(fetch_dirs "${remote_root_dotfiles}")
-
 # Install root dotfiles
 for dotfile in $remote_root_dotfile_names; do
     rm -rf "$HOME/.$dotfile"
@@ -143,11 +144,11 @@ for dotfile in $remote_root_dotfile_names; do
 
     # Check for bad exit code from copy
     if [ $? -ne 0 ]; then
-        echo -e "$Red[Failed]$Color_Off local '.$dotfile' sync."
+        echo -e "\n$Red[Failed]$Color_Off local '.$dotfile' sync.\n"
         exit
     fi
 
-    echo -e "$Green[SUCCESS]$Color_Off local '.$dotfile' is in sync with remote."
+    echo -e "\n$Green[SUCCESS]$Color_Off local '.$dotfile' is in sync with remote.\n"
 done
 
 # Provision config
@@ -157,12 +158,23 @@ for name in $remote_names; do
 
     # Check for bad exit code by rsync
     if [ $? -ne 0 ]; then
-        echo -e "$Red[Failed]$Color_Off local '.config/$name' sync."
+        echo -e "\n$Red[Failed]$Color_Off local '.config/$name' sync.\n"
         exit
     fi
 
-    echo -e "$Green[SUCCESS]$Color_Off local '.config/$name' is in sync with remote."
+    echo -e "\n$Green[SUCCESS]$Color_Off local '.config/$name' is in sync with remote.\n"
 done
+
+echo $OS
+
+if [ $OS == "Darwin" ]; then
+	echo 'eval "$(oh-my-posh init zsh --config $HOME/.config/oh-my-posh/catppuccin_custom.omp.json)"' >> "$HOME/.zshrc"
+	echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+elif [ $OS == "Linux" ]; then
+	echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> "$HOME/.zshrc"
+	echo 'eval "$(oh-my-posh init zsh --config /home/josh/.config/oh-my-posh/catppuccin_custom.omp.json)' >> "$HOME/.zshrc"
+	echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+fi
 
 # Show local dirs not present in remote
 detect_untracked "${local_names}" "${remote_names}"
